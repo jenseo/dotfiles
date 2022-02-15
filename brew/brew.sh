9c787c3cf5c8350c98646f753ab676b0c58b5991
Jens Filipsson <jensfilipsson@gmail.com>
2022-02-15 16:41:53 +0100

    Updated brew script

--- brew/brew.sh
+++ brew/brew.sh
@@ -3,6 +3,10 @@
 # Install Homebrew
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jensfilipsson/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew update
 brew upgrade
 
