a6d30d9c17eddef1948974220e718d6f85d23930
Jens Filipsson <jensfilipsson@gmail.com>
2022-02-19 15:43:02 +0100

    Added .hushlogin to remoce last login message on terminal startup

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ DOTFILES_DIR=~/dotfiles # dotfiles directory
 ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfv "$DOTFILES_DIR/hush-terminal/.hushlogin" ~
 
 
 # Create .nvm directory if it doesn't exist
